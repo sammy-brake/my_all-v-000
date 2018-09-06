440bc22c6b96a00ab28da3071b50d7287c18b235
Samantha brake <sammy.brake@gmail.com>
2018-09-06 23:13:23 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,13 @@ def my_all?(collection)
   block_return_values = []
 i = 0
 while i < collection.length
-yield(collection[i])
+block_return_values << yield(collection[i])
 i += 1
 end
+
+if block_return_values.include?(false) 
+  false 
+else 
+  true 
+end 
 end 
